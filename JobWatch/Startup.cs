1cc4d04280af2d659e54e9eb7f9359f3854b6649
Andrew Tang <Andrew Tang>
2016-03-17 13:32:05 -0500

    first commit. set up vs solution and created database tables documentation

---
+++ JobWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobWatch.Startup))]
+namespace JobWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
