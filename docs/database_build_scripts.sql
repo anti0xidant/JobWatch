b279571b6a50753205e8e58c5962dc2fbc3f61cc
Andrew Tang <atang.business@gmail.com>
2016-03-27 18:16:22 -0500

    modified database_build_scripts.sql

---
+++ docs/database_build_scripts.sql
@@ -0,0 +1,88 @@
+--*****Build Database*****
+USE master
+CREATE DATABASE JobWatch
+GO
+--*************************
+USE JobWatch
+
+CREATE TABLE Company
+(
+CompanyID				int					IDENTITY(1,1)	PRIMARY KEY,
+CompanyName				varchar(30)			not null,
+CompanyUrl				nvarchar(2083)
+)
+
+CREATE TABLE Recruiter
+(
+RecruiterID				int					IDENTITY(1,1)	PRIMARY KEY,
+RecruiterCompanyName	varchar(30)			not null,
+RecruiterName			varchar(30),
+RecruiterNumber			varchar(10),
+RecruiterEmail			varchar(30)
+)
+
+CREATE TABLE Type
+(
+TypeID					int					IDENTITY(1,1)	PRIMARY KEY,
+Type					nchar(20)			not null
+)
+
+CREATE TABLE Location
+(
+LocationID				int					IDENTITY(1,1)	PRIMARY KEY,
+City					varchar(30)			not null,
+State					char(2)				not null
+)
+
+CREATE TABLE IntermediatePhase
+(
+IntermediatePhaseID		int					IDENTITY(1,1)	PRIMARY KEY,
+IntermediatePhase		varchar(25)			not null
+)
+
+CREATE TABLE OfferPhase
+(
+OfferPhaseID			int					IDENTITY(1,1)	PRIMARY KEY,
+OfferPhase				varchar(25)			not null,
+)
+
+CREATE TABLE Language
+(
+LanguageID				int					IDENTITY(1,1)	PRIMARY KEY,
+Language				varchar(20)			not null
+)
+
+CREATE TABLE Jobs
+(
+JobID					int					IDENTITY(1,1)	PRIMARY KEY,
+CompanyID				int,
+RecruiterID				int,
+Title					varchar(30)			not null,
+Url						nvarchar(2083),
+LocationID				int					not null,
+Description				nvarchar(4000),
+DateListed				datetime,
+DateApplied				datetime			not null,
+IsRecruiter				bit					not null,
+IsActive				bit					not null,
+IntermediatePhaseID		int					not null,
+InterviewCount			int					not null,
+OfferPhaseID			int					not null,
+OfferedSalary			smallmoney,
+RejectionStatus			bit					not null,
+
+CONSTRAINT FK_CompanyID				FOREIGN KEY (CompanyID)				REFERENCES	Company				(CompanyID),
+CONSTRAINT FK_RecruiterID			FOREIGN KEY (RecruiterID)			REFERENCES	Recruiter			(RecruiterID),
+CONSTRAINT FK_LocationID			FOREIGN KEY (LocationID)			REFERENCES	Location			(LocationID),
+CONSTRAINT FK_IntermediatePhaseID	FOREIGN KEY (IntermediatePhaseID)	REFERENCES	IntermediatePhase	(IntermediatePhaseID),
+CONSTRAINT FK_OfferPhaseID			FOREIGN KEY (OfferPhaseID)			REFERENCES	OfferPhase			(OfferPhaseID)
+)
+
+CREATE TABLE JobxLanguage
+(
+JobID					int					not null,
+LanguageID				int					not null,
+
+CONSTRAINT FK_JobID			 FOREIGN KEY (JobID)		REFERENCES	Jobs		(JobID),
+CONSTRAINT FK_LanguageID	 FOREIGN KEY (LanguageID)	REFERENCES	Language	(LanguageID)
+)
